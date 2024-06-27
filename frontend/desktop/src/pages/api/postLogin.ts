9ef744c8673e1d789e132f01c192705ae2280ed9
洋愤愤 <1141736487@qq.com>
2024-06-27 16:27:19 +0800

    ssologin1

---
+++ frontend/desktop/src/pages/api/postLogin.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { jsonRes } from '@/services/backend/response';
+import * as crypto from 'crypto';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { name,password}=req.body
+   
+   if(name && password){
+      const body={
+        name:name,
+        password:password
+      }
+      const infoStr=  btoa(JSON.stringify(body));
+      console.log('infoStr',infoStr)
+      res.redirect(`/ssoLogin?infoStr=${infoStr}`)
+      return;
+    }
+    return jsonRes(res, {
+      message: 'Failed to get postLogin',
+      code: 500
+    });
+  } catch (err) {
+    console.log(err);
+    return jsonRes(res, {
+      message: 'Failed to get info',
+      code: 500
+    });
+  }
+}
--- frontend/desktop/src/pages/ssoLogin.tsx
+++ frontend/desktop/src/pages/ssoLogin.tsx
@@ -21,59 +21,61 @@ const setSession = useSessionStore((s) => s.setSession);
 
   useEffect( () => {
     const fetchData = async () => {
-       const name =router.query.name as string
-       const password=router.query.password as string
-       if (name &&password) {
-         try {
-           const inviterId = getInviterId();
-           const result = await passwordExistRequest({ user: name });
-   
-           if (result?.code === 200) {
-             const result = await passwordLoginRequest({
-               user: name,
-               password: password,
-               inviterId
-             });
-             if (!!result?.data) {
-              console.log('sessionConfig')
-               await sessionConfig(result.data);
-             }
-             console.log('sessionConfig success')
-             await router.replace('/');
-           } else if (result?.code === 201) {
-               const regionResult = await passwordLoginRequest({
-                 user: name,
-                 password: password,
-                 inviterId
-               });
-               if (!!regionResult?.data) {
-                 setToken(regionResult.data.token);
-                 const infoData = await UserInfo();
-                 const payload = jwtDecode<AccessTokenPayload>(regionResult.data.token);
-                 setSession({
-                   token: regionResult.data.token,
-                   user: {
-                     k8s_username: payload.userCrName,
-                     name: infoData.data?.info.nickname || '',
-                     avatar: infoData.data?.info.avatarUri || '',
-                     nsid: payload.workspaceId,
-                     ns_uid: payload.workspaceUid,
-                     userCrUid: payload.userCrUid,
-                     userId: payload.userId,
-                     userUid: payload.userUid
-                   },
-                   // @ts-ignore
-                   kubeconfig: result.data.kubeconfig
-                 });
-                 await router.replace('/');
-               }
-             
-           }
-         } catch (error: any) {
-          await router.replace('/signin');
-          console.log('error')
-
-         } 
+       const infoStr =router.query.infoStr as string
+       if(infoStr){
+        const {name,password} =JSON.parse(atob(infoStr))
+        if (name &&password) {
+          try {
+            const inviterId = getInviterId();
+            const result = await passwordExistRequest({ user: name });
+    
+            if (result?.code === 200) {
+              const result = await passwordLoginRequest({
+                user: name,
+                password: password,
+                inviterId
+              });
+              if (!!result?.data) {
+               console.log('sessionConfig')
+                await sessionConfig(result.data);
+              }
+              console.log('sessionConfig success')
+              await router.replace('/');
+            } else if (result?.code === 201) {
+                const regionResult = await passwordLoginRequest({
+                  user: name,
+                  password: password,
+                  inviterId
+                });
+                if (!!regionResult?.data) {
+                  setToken(regionResult.data.token);
+                  const infoData = await UserInfo();
+                  const payload = jwtDecode<AccessTokenPayload>(regionResult.data.token);
+                  setSession({
+                    token: regionResult.data.token,
+                    user: {
+                      k8s_username: payload.userCrName,
+                      name: infoData.data?.info.nickname || '',
+                      avatar: infoData.data?.info.avatarUri || '',
+                      nsid: payload.workspaceId,
+                      ns_uid: payload.workspaceUid,
+                      userCrUid: payload.userCrUid,
+                      userId: payload.userId,
+                      userUid: payload.userUid
+                    },
+                    // @ts-ignore
+                    kubeconfig: result.data.kubeconfig
+                  });
+                  await router.replace('/');
+                }
+              
+            }
+          } catch (error: any) {
+           await router.replace('/signin');
+           console.log('error',error)
+ 
+          } 
+        }
        }
        
     };
